64d9d63b78b7d8d01badf6ebbd12c197f03a652a
devmatsuko <devmatsuko@gmail.com>
2021-01-29 21:27:47 +0900

    [Add]ルートパスの追加。

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// ブログ一覧画面を表示(名前付きURL:blogs)
+Route::get('/', 'BlogController@showList')->name("blogs");
